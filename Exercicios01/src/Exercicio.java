cc61ceef6b1b051e084f94f8e47b7b5c49eeb853
˜Seu <jorge.desouza@mercadolivre.com>
2022-10-29 22:56:55 -0300

    Inclusão de dois exercícios.

---
+++ Exercicios01/src/Exercicio.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ *Faça um programa para imprimir uma tabuada.
+ *******************************************************************************/
+import java.util.Scanner;
+
+public class Exercicio {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double numero;
+        System.out.print("Digite um número: ");
+        numero = entrada.nextDouble();
+        System.out.println();
+
+        for(int x = 0; x <= 10; x ++){
+
+            System.out.printf("%f x %d \n", numero,x, numero*x);
+        }
+
+
+    }
+}
+
